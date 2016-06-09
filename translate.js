1b33e76680224c19a6cccbfabd44c2c4328e5147
mekhatria <mustapha@highsoft.com>
2016-06-10 00:43:30 +0200

    V1.0
    + Correct English and Norsk text

--- translate.js
+++ translate.js
@@ -13,9 +13,11 @@ app.config(function($translateProvider) {
         MENU_DONATION:'Donation',
         MENU_CONTACT_US:'Contact us',    
         PURPOSE_PARAGRAPH:'LevSaman assists refugees and other people of concern to integrate in the Norwegian society by providing them the needed trainings and coaching',
-        ABOUT_US_PARAGRAPH:'We believe that we can live all together, and the best way is by helping each other as much as we can. with the right training and coaching, every individual will be a positive elements for Norway :)',
-        EVENTS_PARAGRAPH:'Join us each Friday from 19:00 to 21:00 for free driving lesson and IT/english training',
+        ABOUT_US_PARAGRAPH:'We believe that we can live all together, and the best way is by helping each other as much as we can. With the right training and coaching, every individual will be a positive element for the Norwegian society :)',
+        EVENTS_PARAGRAPH:'Join us each Friday from 19:00 to 21:00 for free driving lesson and/or IT/english training',
         DONATION_PARAGRAPH:'You can also make a difference in your community by supporting LevSaman:',
+        BANK_INFORMATION:'Bank information',
+        ADDRESS:'Address',
         CONTACT_US_PARAGRAPH:'',
         SPONSOR:'Sponsor'
 	})
@@ -27,13 +29,15 @@ app.config(function($translateProvider) {
         MENU_ABOUT_US:'Om oss',
         MENU_EVENTS:'Hendingar',
         MENU_DONATION:'Donasjon',
-        MENU_CONTACT_US:'kontakt oss',    
+        MENU_CONTACT_US:'Kontakt oss',    
         PURPOSE_PARAGRAPH:'LevSaman hjelper flyktningar og andre som treng det med integrering i det norske samfunnet ved å tilby naudsynt opplæring og rettleiing',
-        ABOUT_US_PARAGRAPH:'Vi trur at vi kan leve saman, og den beste måten er ved å hjelpe kvarandre så mykje som mogleg. med rett opplæring og rettleiing vil kvar einskild kunne være eit positivt element for Norge :)',
-        EVENTS_PARAGRAPH:'Bli med oss hver fredag 19:00 til 21:00 for gratis kjøretime og IT / engelsk opplæring',
-        DONATION_PARAGRAPH:'Du kan også gjøre en forskjell i samfunnet ved å støtte LevSaman',
+        ABOUT_US_PARAGRAPH:'Vi trur at vi kan leve saman, og den beste måten er ved å hjelpe kvarandre så mykje som mogleg. Med rett opplæring og rettleiing vil kvar einskild kunne være eit positivt element for det norske samfunnet :)',
+        EVENTS_PARAGRAPH:'Bli med oss kvar fredag frå kl 19:00 til 21:00 for gratis kjøretime og IT / engelsk opplæring',
+        DONATION_PARAGRAPH:'Du kan også gjere ein forskjell i samfunnet ved å støtte LevSaman:',
+        BANK_INFORMATION:'Kontonummer',
+        ADDRESS:'Adresse',
         CONTACT_US_PARAGRAPH:'',
-        SPONSOR:'Sponsorar'
+        SPONSOR:'Sponsor'
 	});
 
 	$translateProvider.preferredLanguage('no');
